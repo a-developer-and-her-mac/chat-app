05bf1fa54302dc57a921ef3921a77bd3e42fee41
a-developer-and-her-mac <64797765+a-developer-and-her-mac@users.noreply.github.com>
2021-01-21 14:51:24 -0800

    Refactor: update the uploadImage function to use arrow functions

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -75,10 +75,10 @@ export default class CustomActions extends React.Component {
     try {
       const blob = await new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
-        xhr.onload = function() {
+        xhr.onload = () => {
           resolve(xhr.response);
         };
-        xhr.onerror = function(e) {
+        xhr.onerror = (e) => {
           console.log(e);
           reject(new TypeError('Network request failed'));
         };
